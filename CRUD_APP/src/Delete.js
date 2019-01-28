3e49e69ad27ad66f408f15b3a63bb46d580b21c9
bacancy-neel <neel.suthar@bacancytechnology.com>
2019-01-28 15:44:20 +0530

    delete component optimized

--- CRUD_APP/src/Delete.js
+++ CRUD_APP/src/Delete.js
@@ -3,7 +3,8 @@ import React, { Component } from "react";
 class Delete extends Component {
 
   render() {
-    window.confirm("Are you sure?") ? this.props.history.goBack() : window.alert("Not deleted");
+    window.confirm("Are you sure?") ? window.alert("User deleted") : window.alert("User not deleted");
+    this.props.history.goBack();
     return (
 
       < React.Fragment ></React.Fragment >
